6e9013d9bba14168701b456c9dd9aa067e90c104
absk786 <rehmank11@gmail.com>
2021-09-10 12:42:22 +0100

    hpdaasdf

---
+++ server/schemas/index.js
@@ -0,0 +1,8 @@
+//import the gql  tagged implemented fxn 
+const {gql} = require('apollo-server-express')
+
+//create typedefs
+const typeDefs = gql ``;
+
+//export typedegs
+module.exports = typeDefs;
