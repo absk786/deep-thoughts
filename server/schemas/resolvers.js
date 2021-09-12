682831788522497a43548e1482a3589d07594aa2
absk786 <rehmank11@gmail.com>
2021-09-12 11:39:21 +0100

    updated

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,26 @@
+const {User, Thought} = require('../models')
+
 const resolvers = { 
     Query: {
-        helloWorld: () => {
-            return 'Hello World';
-        }
-    }
+        //get all users
+        users: async () => {
+            return User.find().select('-__v -password').populate('friends').populate('thoughts');
+        },
+        //get users by usernmae
+        user: async (parent, {username}) => {
+            return User.findOne({username}).select('-__v -passwrod').populate('friends').populate('thoughts')
+        },
+        //get all thoughts
+        thoughts: async (parent, {username}) => {
+            const params = username? {username} : {};
+            return Thought.find(params).sort({createdAt: -1}) ;
+        },
+        //get thought by id
+        thought: async (parent, _id) => {
+            return Thought.findOne({_id})
+        },
+     }
 };
+
+// Now when we query thoughts, we will perform a .find() method on the Thought model. We're also returning the thought data in descending order, as can be seen in the .sort() method that we chained onto it. 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,10 +3,40 @@ const {gql} = require('apollo-server-express')
 
 //create typedefs
 const typeDefs = gql `
-    type Query {
-        helloWorld: String
+type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+ type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+type Query {
+        users:[User]
+        user(username:String!):User
+        thoughts(username:String): [Thought] 
+        thought(_id:ID!): Thought
     }
 `;
 
-//export typedegs
+//we're instructing our query that we'll return an array, as noted by the [] square brackets around the returning data, Thought
+//With this in place, we have now set it up so that when we run the thoughts query, we can also list the reactions field to get back an array of reaction data for each thought
+
+
 module.exports = typeDefs;
